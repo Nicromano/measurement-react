389f92fd35e70537f5875a4372a6cb2d780505f6
Nicromano <ljose297@gmail.com>
2021-01-26 22:19:12 -0500

    Fondo 2

--- src/components/Measurements/Measurements.js
+++ src/components/Measurements/Measurements.js
@@ -6,7 +6,7 @@ class mesaurements extends React.Component {
       <div className="card m-5 p-2 shadow">
         <div className="card-body">
           <h5 className="card-title text-center">Cálculo de medidas</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <h6 className="card-subtitle mb-2 text-muted"></h6>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
